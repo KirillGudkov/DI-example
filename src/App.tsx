e027ce75241c57e6a69b4aed1ff0e9adc1fc00ac
Kirill Gudkov <for.example.nickname@gmail.com>
2019-01-07 02:44:16 +0300

    refactoring, update in-app package

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {Fragment} from 'react'
-import {Image, StatusBar, StyleSheet, View} from 'react-native';
+import {Image, StatusBar, StyleSheet, Animated, TouchableOpacity, View} from 'react-native';
 import {Provider} from 'react-redux';
 import {store} from "./Config/reduxStoreConfig";
 import {AppNavigator} from "./Navigation/router";
@@ -10,10 +10,11 @@ import {observer} from "mobx-react";
 import {LightTheme} from "./MobX/LightTheme";
 import {Notification} from "react-native-in-app-message";
 import {TextView} from "./TextView";
+import {BlurView} from "react-native-blur";
 
 DIBuilder.build(DITypes);
 
-const avatar = {uri: 'https://i.pinimg.com/736x/ea/f2/2e/eaf22e6a189f76ed24054e2ca7feb00f.jpg'};
+const avatar = {uri: 'https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2018/10/18/Pictures/_5fb51944-d2ee-11e8-841e-211dfd3178e1.jpg'};
 
 interface Props {
   themeStore: ThemeStore
@@ -22,17 +23,37 @@ interface Props {
 @observer
 class Application extends React.Component<Props, {}> {
 
+
+  @bind
+  onOkPress() {
+
+  }
+
   private renderCustomNotification(color: string) {
     return (
-      <View style={style.notificationContainer}>
-        <Image source={avatar} style={style.avatar} />
-        <View style={style.textContainer}>
-          <TextView style={{color: color, fontWeight: '600'}}>Tony Stark</TextView>
-          <TextView style={{color: color, fontSize: 14}}>Hey how's it going? I'm looking for you all day!</TextView>
+      <View style={style.notification}>
+        <View style={style.notificationContainer}>
+          <Image resizeMethod={'resize'} source={avatar} style={style.avatar} />
+          <View style={style.textContainer}>
+            <TextView style={{color: color, fontWeight: '600'}}>Iron Man</TextView>
+            <TextView style={{color: color, fontSize: 14}}>Iron Man (Anthony Edward "Tony" Stark) is a fictional superhero appearing in American comic books published by Marvel Comics.</TextView>
+          </View>
+        </View>
+        <View style={style.buttonsContainer}>
+          <BlurView style={style.leftButtonBlur} blurAmount={10} blurType={'light'} />
+          <TouchableOpacity onPress={() => Notification.hide()} style={style.button}>
+            <TextView style={{color, fontSize: 15}}>Close</TextView>
+          </TouchableOpacity>
+          <View style={[style.buttonDivider, {backgroundColor: color}]} />
+          <BlurView style={style.rightButtonBlur} blurAmount={10} blurType={'light'} />
+          <TouchableOpacity onPress={this.onOkPress} style={style.button}>
+            <TextView style={{color, fontSize: 15}}>Read more</TextView>
+          </TouchableOpacity>
         </View>
       </View>
     )
   }
+
   render(): React.ReactNode {
     const {themeStore} = this.props;
     const {color, backgroundColor, accentColor} = themeStore.theme;
@@ -42,6 +63,7 @@ class Application extends React.Component<Props, {}> {
         <StatusBar barStyle={barStyle} />
         <AppNavigator screenProps={{themeStore, color, backgroundColor, accentColor}} />
         <Notification
+          showKnob={false}
           onPress={() => Notification.hide()}
           autohide={false}
           textColor={color}
@@ -54,19 +76,64 @@ class Application extends React.Component<Props, {}> {
 const style = StyleSheet.create({
   notificationContainer: {
     flex: 1,
-    marginHorizontal: 8,
-    flexDirection: 'row',
+    width: '100%',
+    marginHorizontal: 12,
     alignItems: 'center'
   },
+  buttonsContainer: {
+    flex: 1,
+    marginHorizontal: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+    width: '100%'
+  },
+  notification: {
+    borderRadius: 12,
+    overflow: 'hidden',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   avatar: {
-    width: 60,
-    height: 60,
-    borderRadius: 30
+    borderTopRightRadius: 12,
+    borderTopLeftRadius: 12,
+    width: '100%',
+    height: 150,
   },
   textContainer: {
-    flex: 1,
+    width: '100%',
+    paddingHorizontal: 8,
+    marginTop: 12,
     marginLeft: 8
-  }
+  },
+  leftButtonBlur: {
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+    width: '50%',
+    height: '100%',
+    opacity: 0.3,
+  },
+  rightButtonBlur: {
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+    width: '50%',
+    height: '100%',
+    opacity: 0.3,
+  },
+  button: {
+    flex: 1,
+    height: 36,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonDivider: {
+    width: 1,
+    height: 20,
+    opacity: 0.3
+  },
 });
 
 const _App = () => (
